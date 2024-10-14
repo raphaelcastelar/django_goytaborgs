6b1b8bf7d3cb14eed685388a3c71347469123410
raphael castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-10-14 20:49:49 -0300

    a

--- django_goytaborgs/equipe/models.py
+++ django_goytaborgs/equipe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Equipe(models.Model):
+    nome = models.CharField(max_length=100)
+    capitao = models.CharField(max_length=100)
+    data_criacao = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
